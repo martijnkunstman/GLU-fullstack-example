6715b45d196d267d5e40532bbd353ccd4063a826
martijnkunstman <martijn@kunstman.nl>
2019-06-18 13:44:20 +0200

    auto build

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,6 +1,6 @@
-const path = require("path")
+const path = require('path')
 const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
+const HtmlWebPackPlugin = require('html-webpack-plugin')
 module.exports = {
   entry: {
     main: './src/index.js'
@@ -10,6 +10,7 @@ module.exports = {
     publicPath: '/',
     filename: '[name].js'
   },
+  mode: 'development',
   target: 'web',
   devtool: 'source-map',
   module: {
@@ -30,7 +31,7 @@ module.exports = {
           }
         ]
       },
-      {
+      { 
         test: /\.css$/,
         use: [ 'style-loader', 'css-loader' ]
       },
@@ -45,6 +46,7 @@ module.exports = {
       template: "./src/html/index.html",
       filename: "./index.html",
       excludeChunks: [ 'server' ]
-    })
+    }),
+    new webpack.NoEmitOnErrorsPlugin()
   ]
 }
